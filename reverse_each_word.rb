b797418f80ba2ec124c881b42fdb015c427035e5
sydney miranda <sydmiranda@icloud.com>
2021-01-06 02:59:39 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,8 @@
+def reverse_each_word(sentence)
+  # reverse_each_word("Hello there, and how are you?")
+  #use .each & .collect
+ # turn string into array with split
+ # use each on every word in array and reverse
+ # turn array into string with join
+sentence.split.collect {|word| word.reverse}.join(" ")
+end
