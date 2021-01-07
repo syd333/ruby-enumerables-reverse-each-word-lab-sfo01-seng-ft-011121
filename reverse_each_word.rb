7b1e8a9e129389f3a5340462566afd2bb2c9989b
sydney miranda <sydmiranda@icloud.com>
2021-01-07 01:42:37 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   # reverse_each_word("Hello there, and how are you?")
   #use .each & .collect
  # turn string into array with split
- # use each on every word in array and reverse
+ # use collect on every word in array and reverse
  # turn array into string with join
 sentence.split.collect {|word| word.reverse}.join(" ")
 end
